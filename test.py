f433deb918deafe8c923e388e56cfab3cd5047fa
yodah <None>
2024-07-07 21:44:30 +0200

    test: Add tests in test.py

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ from pathlib import Path
 
 
 def test_patch(original_path: Path, modified_path: Path) -> None:
-    patch_path = original_path.with_stem(original_path.stem + "-patch-v6").with_suffix(".png") # must be lossless
-    patched_path = original_path.with_stem(original_path.stem + "-patched-v6").with_suffix(modified_path.suffix)
-    reversed_path = original_path.with_stem(original_path.stem + "-reversed-v6").with_suffix(modified_path.suffix)
-    difference_path = original_path.with_stem(original_path.stem + "-difference-v6").with_suffix(modified_path.suffix)
+    patch_path = original_path.with_stem(original_path.stem + "-patch-v8").with_suffix(".png") # must be lossless
+    patched_path = original_path.with_stem(original_path.stem + "-patched-v8").with_suffix(modified_path.suffix)
+    reversed_path = original_path.with_stem(original_path.stem + "-reversed-v8").with_suffix(modified_path.suffix)
+    difference_path = original_path.with_stem(original_path.stem + "-difference-v8").with_suffix(modified_path.suffix)
     create_patch(original_path, modified_path, patch_path)
     create_patched(original_path, patch_path, patched_path)
     if (difference := compare_image(modified_path, patched_path, difference_path)) != (0, 0):
@@ -21,10 +21,12 @@ def test_patch(original_path: Path, modified_path: Path) -> None:
 
 
 def test() -> None:
+    test_patch(Path("./demo/hud-powercell.png"), Path("./demo/hud-powercell-modified.png"))
+    test_patch(Path("./demo/assis-choker.png"), Path("./demo/assis-choker-modified.png"))
     test_patch(Path("./demo/dc-handle-02.png"), Path("./demo/dc-handle-02-modified.png"))
-    # test_patch(Path("./demo/buzzerfly-icon.png"), Path("./demo/buzzerfly-icon-modified.png"))
-    # test_patch(Path("./demo/crate-brown-wood.jpg"), Path("./demo/crate-brown-wood-modified.png"))
-    # test_patch(Path("./demo/duion-art-photos-CF_DSC05592.jpg"), Path("./demo/duion-art-photos-CF_DSC05592-modified.jpg"))
+    test_patch(Path("./demo/buzzerfly-icon.png"), Path("./demo/buzzerfly-icon-modified.png"))
+    test_patch(Path("./demo/crate-brown-wood.jpg"), Path("./demo/crate-brown-wood-modified.png"))
+    test_patch(Path("./demo/duion-art-photos-CF_DSC05592.jpg"), Path("./demo/duion-art-photos-CF_DSC05592-modified.jpg"))
 
 if __name__ == "__main__":
     test()
