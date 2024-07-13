ac8bedb7615b46198a320d534a28d5a4fe0a04d2
yodah <None>
2024-07-13 15:28:47 +0200

    refactor: Use path arguments in main.py

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ from pack import create_texture_pack, create_texture_patch_pack
 from filters import filter_image, FITLER_NAMES
 
 
-def create(original_path_str: str, modified_path_str: str, patch_path_str: str):
-    original_path = Path(original_path_str)
-    modified_path = Path(modified_path_str)
-    patch_path = Path(patch_path_str)
+def create(original_path: Path, modified_path: Path, patch_path: Path):
     if original_path.is_file() and modified_path.is_file():
         create_patch(original_path, modified_path, patch_path)
     elif original_path.is_dir() and modified_path.is_dir():
@@ -20,10 +17,7 @@ def create(original_path_str: str, modified_path_str: str, patch_path_str: str):
         print("Expected either all directories or all images")
 
 
-def apply(original_path_str: str, patch_path_str: str, patched_path_str: str):
-    original_path = Path(original_path_str)
-    patch_path = Path(patch_path_str)
-    patched_path = Path(patched_path_str)
+def apply(original_path: Path, patch_path: Path, patched_path: Path):
     if original_path.is_file() and patch_path.is_file():
         create_patched(original_path, patch_path, patched_path)
     elif original_path.is_dir() and patch_path.is_dir():
@@ -32,10 +26,7 @@ def apply(original_path_str: str, patch_path_str: str, patched_path_str: str):
         print("Expected either all directories or all images")
 
 
-def diff(reference_path_str: str, patched_path_str: str, difference_path_str: str|None):
-    reference_path = Path(reference_path_str)
-    patched_path = Path(patched_path_str)
-    difference_path = Path(difference_path_str) if difference_path_str else None
+def diff(reference_path: Path, patched_path: Path, difference_path: Path|None):
     if reference_path.is_file() and patched_path.is_file():
         print(compare_image(reference_path, patched_path, difference_path))
     elif reference_path.is_dir() and patched_path.is_dir():
@@ -45,10 +36,7 @@ def diff(reference_path_str: str, patched_path_str: str, difference_path_str: st
         print("Expected either all directories or all images")
 
 
-def reverse(modified_path_str: str, patch_path_str: str, reversed_path_str: str|None):
-    modified_path = Path(modified_path_str)
-    patch_path = Path(patch_path_str)
-    reversed_path = Path(reversed_path_str)
+def reverse(modified_path: Path, patch_path: Path, reversed_path: Path|None):
     # TODO: mk parents
     if modified_path.is_file() and patch_path.is_file():
         reverse_original(modified_path, patch_path, reversed_path)
@@ -59,9 +47,7 @@ def reverse(modified_path_str: str, patch_path_str: str, reversed_path_str: str|
         print("Expected either all directories or all images")
 
 
-def test(original_path_str: str, modified_path_str: str):
-    original_path = Path(original_path_str)
-    modified_path = Path(modified_path_str)
+def test(original_path: Path, modified_path: Path):
     if original_path.is_file() and modified_path.is_file():
         test_patch(original_path, modified_path)
     elif original_path.is_dir() and modified_path.is_dir():
@@ -79,45 +65,45 @@ def main():
     subparsers = parser.add_subparsers(dest="subparser_name", help="sub-command help")
     
     create_parser = subparsers.add_parser("create", help="Create a patch")
-    create_parser.add_argument(dest="original_path",                   metavar="original-path", type=str, # "-i", "--input", default=".",
+    create_parser.add_argument(dest="original_path",                   metavar="original-path", type=Path, # "-i", "--input", default=".",
         help="The path to the original directory or image")
-    create_parser.add_argument(dest="modified_path",                   metavar="modified-path", type=str, # "-m", "--modified", default=DEFAULT_OUTPUT_PATH,
+    create_parser.add_argument(dest="modified_path",                   metavar="modified-path", type=Path, # "-m", "--modified", default=DEFAULT_OUTPUT_PATH,
         help="The path to the modified directory or image")
-    create_parser.add_argument(dest="patch_path",                      metavar="patch-path",    type=str, # "-o", "--output", default=DEFAULT_OUTPUT_PATH,
+    create_parser.add_argument(dest="patch_path",                      metavar="patch-path",    type=Path, # "-o", "--output", default=DEFAULT_OUTPUT_PATH,
         help="The path to the directory containing patch images or patch image")
     # --print-from-root
     # -r --max-depth x
 
     apply_parser = subparsers.add_parser("apply", help="Apply a patch")
-    apply_parser.add_argument(dest="original_path",                    metavar="original-path", type=str, # "-i", "--input", default=".",
+    apply_parser.add_argument(dest="original_path",                    metavar="original-path", type=Path, # "-i", "--input", default=".",
         help="The path to the original directory or image")
-    apply_parser.add_argument(dest="patch_path",                       metavar="patch-path",    type=str, # "-o", "--patch", default=DEFAULT_OUTPUT_PATH,
+    apply_parser.add_argument(dest="patch_path",                       metavar="patch-path",    type=Path, # "-o", "--patch", default=DEFAULT_OUTPUT_PATH,
         help="The path to the patch directory or image")
-    apply_parser.add_argument(dest="patched_path",                     metavar="patched-path",  type=str, # "-m", "--output", default=DEFAULT_OUTPUT_PATH,
+    apply_parser.add_argument(dest="patched_path",                     metavar="patched-path",  type=Path, # "-m", "--output", default=DEFAULT_OUTPUT_PATH,
         help="The path to the directory containing patched images or patched image")
     # --print-from-root
     # -r --max-depth x
 
     diff_parser = subparsers.add_parser("diff", help="Compare a reference image with a modified one")
-    diff_parser.add_argument(dest="reference_path",                    metavar="reference-path",  type=str, # "-i", "--input", default=".",
+    diff_parser.add_argument(dest="reference_path",                    metavar="reference-path",  type=Path, # "-i", "--input", default=".",
         help="The path to the reference directory or image")
-    diff_parser.add_argument(dest="modified_path",                     metavar="modified-path", type=str, # "-m", "--modified", default=DEFAULT_OUTPUT_PATH,
+    diff_parser.add_argument(dest="modified_path",                     metavar="modified-path",   type=Path, # "-m", "--modified", default=DEFAULT_OUTPUT_PATH,
         help="The path to the modified directory or image")
-    diff_parser.add_argument(dest="difference_path",                   metavar="difference-path",     type=str, nargs="?", default=None,# "-o", "--output",
+    diff_parser.add_argument(dest="difference_path",                   metavar="difference-path", type=Path, nargs="?", default=None,# "-o", "--output",
         help="The path to the directory containing difference images or difference image")
 
     reverse_parser = subparsers.add_parser("reverse", help="Reverse the original image by a patch")
-    reverse_parser.add_argument(dest="modified_path",                  metavar="modified-path", type=str, # "-m", "--modified", default=DEFAULT_OUTPUT_PATH,
+    reverse_parser.add_argument(dest="modified_path",                  metavar="modified-path",   type=Path, # "-m", "--modified", default=DEFAULT_OUTPUT_PATH,
         help="The path to the modified directory or image")
-    reverse_parser.add_argument(dest="patch_path",                     metavar="patch-path",    type=str, # "-o", "--output", default=DEFAULT_OUTPUT_PATH,
+    reverse_parser.add_argument(dest="patch_path",                     metavar="patch-path",      type=Path, # "-o", "--output", default=DEFAULT_OUTPUT_PATH,
         help="The path to the patch directory or image")
-    reverse_parser.add_argument(dest="reversed_path",                  metavar="reversed-path", type=str, # "-i", "--input", default=".",
+    reverse_parser.add_argument(dest="reversed_path",                  metavar="reversed-path",   type=Path, # "-i", "--input", default=".",
         help="The path to the directory containing reversed images or reversed image")
 
     test_parser = subparsers.add_parser("test", help="Run all modes on an original and its modified image")
-    test_parser.add_argument(dest="original_path",                     metavar="original-path", type=str, # "-i", "--input", default=".",
+    test_parser.add_argument(dest="original_path",                     metavar="original-path",   type=Path, # "-i", "--input", default=".",
         help="The path to the original directory or image")
-    test_parser.add_argument(dest="modified_path",               metavar="modified-path", type=str, # "-m", "--modified", default=DEFAULT_OUTPUT_PATH,
+    test_parser.add_argument(dest="modified_path",                     metavar="modified-path",   type=Path, # "-m", "--modified", default=DEFAULT_OUTPUT_PATH,
         help="The path to the modified directory or image")
 
     filter_parser = subparsers.add_parser("test-filter", help=" all modes on an original and its modified image")
