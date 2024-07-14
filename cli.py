3b8f066c5b1187cf5bf239e092cc178f430e0051
yodah <None>
2024-07-14 17:44:04 +0200

    refactor: Add cli.py

---
+++ cli.py
@@ -0,0 +1,9 @@
+
+RESET = "\x1b[0m"
+RED = "\x1b[1;31m"
+GREEN = "\x1b[1;32m"
+ORANGE = "\x1b[1;33m"
+BLUE = "\x1b[1;34m"
+MAGENTA = "\x1b[1;35m"
+CYAN = "\x1b[1;36m"
+BOLD = "\x1b[1;37m"
--- pack.py
+++ pack.py
@@ -2,19 +2,11 @@ from pathlib import Path
 from typing import Callable #, Optional
 from patch import create_patch, create_patched
 from difference import compare_image
+from cli import RESET, RED, GREEN, ORANGE, BLUE, MAGENTA, CYAN, BOLD
 
 
 INDENTATION = "  " # "\t"
 SUFFIXES = [".png"]
-
-RESET = "\x1b[0m"
-RED = "\x1b[1;31m"
-GREEN = "\x1b[1;32m"
-ORANGE = "\x1b[1;33m"
-BLUE = "\x1b[1;34m"
-MAGENTA = "\x1b[1;35m"
-CYAN = "\x1b[1;36m"
-BOLD = "\x1b[1;37m"
 
 
 def print_indented(text: str, level: int, end: str|None = None, flush: str|None = None) -> None:
